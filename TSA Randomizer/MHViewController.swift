7cc2d3d6dd6c51e901e787205c6ee93f7130c0a6
Michael Hulet <bestraysfan@me.com>
2016-04-15 21:33:07 -0400

    Finished the MVP in like 8 minutes

--- TSA Randomizer/MHViewController.swift
+++ TSA Randomizer/MHViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
+enum MHArrowImages: String{
+    case Left = "left-arrow"
+    case Right = "right-arrow"
+}
+
 class ViewController: UIViewController{
+    @IBOutlet weak var arrowView: UIImageView!
+    @IBOutlet weak var directionLabel: UILabel!
     override func viewDidLoad() -> Void{
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        randomize()
     }
     override func didReceiveMemoryWarning() -> Void{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) -> Void{
+        randomize()
+    }
+    func randomize() -> Void{
+        if Bool(round(Double(arc4random()) / Double(UINT32_MAX))){
+            arrowView.image = UIImage(named: MHArrowImages.Left.rawValue)
+            directionLabel.text = "LEFT"
+        }
+        else{
+            arrowView.image = UIImage(named: MHArrowImages.Right.rawValue)
+            directionLabel.text = "RIGHT"
+        }
     }
 }
